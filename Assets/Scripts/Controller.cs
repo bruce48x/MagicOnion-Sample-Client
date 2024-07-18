2649dd48bf3469a6570c96884f4b5a49a18baff7
bruce <bruce48@foxmail.com>
2024-07-18 14:04:47 +0800

    init project

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace SampleClient
+{
+    public class Controller : MonoBehaviour
+    {
+        public float moveSpeed = 5.0f;
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.back * (moveSpeed * Time.deltaTime));
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * (moveSpeed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * (moveSpeed * Time.deltaTime));
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Translate(Vector3.up * (moveSpeed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.Z))
+            {
+                transform.Translate(Vector3.down * (moveSpeed * Time.deltaTime));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SampleUIClient.cs
@@ -0,0 +1,90 @@
+using System;
+using Cysharp.Net.Http;
+using Grpc.Core;
+using Shared.Interfaces;
+using Grpc.Net.Client;
+using MagicOnion;
+using MagicOnion.Client;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SampleClient
+{
+    public class SampleUIClient : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerObject;
+        private GamingHubClient _hubClient;
+        private ChannelBase _channel;
+
+        private TextField nameField;
+        private TextField roomField;
+        private bool _isConnected = false;
+
+        private async void Start()
+        {
+            _channel = GrpcChannelx.ForAddress("http://127.0.0.1:5001/");
+
+            var serviceClient = MagicOnionClient.Create<IMyFirstService>(_channel);
+            var result = await serviceClient.SumAsync(100, 200);
+            Debug.Log(result);
+
+            // UI¥Ü¥¿¥ó¤È™CÄÜ¤ÎßBÐ¯
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            var button = root.Q<Button>("Connect");
+            button.clicked += async () =>
+            {
+                Debug.Log("room Button clicked!");
+                if (_isConnected) return;
+                _hubClient = new GamingHubClient(playerObject);
+                _ = await _hubClient.ConnectAsync(_channel, roomField.value, nameField.value);
+                _isConnected = true;
+                nameField.isReadOnly = true;
+                nameField.isReadOnly = true;
+            };
+
+            var button2 = root.Q<Button>("Disconnect");
+            button2.clicked += async () =>
+            {
+                Debug.Log("name Button clicked!");
+                _isConnected = false;
+                nameField.isReadOnly = false;
+                nameField.isReadOnly = false;
+                await _hubClient.LeaveAsync(playerObject.name);
+                await _hubClient.DisposeAsync();
+            };
+
+            nameField = root.Q<TextField>("name");
+            playerObject.name = nameField.value;
+            nameField.RegisterValueChangedCallback(evt =>
+            {
+                Debug.Log("Entered Name: " + evt.newValue);
+                if (!_isConnected) playerObject.name = evt.newValue;
+            });
+
+            roomField = root.Q<TextField>("room");
+            roomField.RegisterValueChangedCallback(evt =>
+            {
+                Debug.Log("Entered Name: " + evt.newValue);
+                if (_isConnected) roomField.isReadOnly = true;
+            });
+        }
+
+        private async void Update()
+        {
+            if (_hubClient == null) return;
+            if (_isConnected)
+            {
+                var position = playerObject.transform.position;
+                var rotation = playerObject.transform.rotation;
+                await _hubClient.MoveAsync(position, rotation);
+            }
+        }
+
+        private async void OnApplicationQuit()
+        {
+            if (_hubClient == null) return;
+            await _hubClient.LeaveAsync(playerObject.name);
+            await _hubClient.DisposeAsync();
+        }
+    }
+}
